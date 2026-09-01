220313342f6c3960a1a388498ac788be7f3db35b
Nauval Gunawan <nauvalgunawan5@gmail.com>
2026-09-01 19:25:19 +0700

    fix(layout): tambahkan suppressHydrationWarning pada body untuk mencegah mismatch atribut style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default async function RootLayout({
 
   return (
     <html lang="id" className="overflow-hidden">
-      <body className={`${outfit.className} overflow-hidden h-[100dvh] w-screen`}>
+      <body className={`${outfit.className} overflow-hidden h-[100dvh] w-screen`} suppressHydrationWarning>
         <MainContentWrapper user={user} permissions={permissions}>
           {children}
         </MainContentWrapper>
